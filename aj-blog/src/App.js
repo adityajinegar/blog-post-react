135f369ccf9d236cdc9c6a9daa5338225f3ee1a5
Aditya Jinegar <aditya_jinegar@yahoo.com>
2021-06-13 01:43:56 -0400

    Update App.js

--- aj-blog/src/App.js
+++ aj-blog/src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <div className="content">
-        <h1>Hello World</h1>
+        <Home></Home>
       </div>
     </div>
   );
