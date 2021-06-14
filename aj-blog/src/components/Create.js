3154ec0dbc2522a1a6ad59ea9eb5c01fd9fde00c
Aditya Jinegar <aditya_jinegar@yahoo.com>
2021-06-14 15:53:35 -0400

    Update Create.js

--- aj-blog/src/components/Create.js
+++ aj-blog/src/components/Create.js
@@ -1,7 +1,43 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("Naruto");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog Title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        ></input>
+
+        <label>Blog Body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+
+        <label>Blog Author:</label>
+
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="Naruto">Naruto</option>
+          <option value="Sasuke">Sasuke</option>
+          <option value="Minato">Minato</option>
+        </select>
+
+        <button>Add Blog</button>
+      </form>
     </div>
   );
 };
