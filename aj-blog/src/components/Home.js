e769e5bd558c2bfdf7f1a4a22f364deb580d63f0
Aditya Jinegar <aditya_jinegar@yahoo.com>
2021-06-13 20:32:04 -0400

    Update Home.js

--- aj-blog/src/components/Home.js
+++ aj-blog/src/components/Home.js
@@ -3,31 +3,33 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:8000/blogs")
       .then((res) => {
+        if (!res.ok) {
+          throw Error("Could not fetch the data for that resource");
+        }
         return res.json();
       })
       .then((data) => {
         setBlogs(data);
+        setIsLoading(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        setError(error.message);
       });
   }, []);
 
   return (
     <div className="home">
-      {blogs && (
-        <BlogList
-          blogs={blogs}
-          title="All Blogs!"
-          handleDelete={handleDelete}
-        ></BlogList>
-      )}
+      {error && <div>{error}</div>}
+      {isLoading && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!"></BlogList>}
     </div>
   );
 };
