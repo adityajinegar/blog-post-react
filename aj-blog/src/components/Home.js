465924bf0f8e42e4aa3cec47d8c2229734132aa8
Aditya Jinegar <aditya_jinegar@yahoo.com>
2021-06-13 20:39:59 -0400

    Update Home.js

--- aj-blog/src/components/Home.js
+++ aj-blog/src/components/Home.js
@@ -2,28 +2,9 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
 
-  useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        if (!res.ok) {
-          throw Error("Could not fetch the data for that resource");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setBlogs(data);
-        setIsLoading(false);
-        setError(null);
-      })
-      .catch((err) => {
-        setIsLoading(false);
-        setError(error.message);
-      });
-  }, []);
+
+
 
   return (
     <div className="home">
