cba8e7249cf678194698b1606da98aadabaec41f
Aditya Jinegar <aditya_jinegar@yahoo.com>
2021-06-14 17:30:08 -0400

    Update BlogDetails.js

--- aj-blog/src/components/BlogDetails.js
+++ aj-blog/src/components/BlogDetails.js
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import useFetch from "../useFetch";
-import useHistory from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 const BlogDetails = () => {
   const { id } = useParams();
